7c639f79510ae6819875636c3996cce81ad1afba
Misael-GC <misael.gomez1623@gmail.com>
2025-11-06 22:29:12 -0600

    se creo todo el sistema desde vista, rutas y controlador la parte de crear un nuevo post con un formulario de creación

--- resources/links.template.php
+++ resources/links.template.php
@@ -9,7 +9,7 @@
 </div>
 
 <div class="grid grid-cols-1 lg:grid-cols-3 gap-x-8 gap-y-16">
-    
+
     <!-- foreach -->
     <?php foreach ($links as $link): ?>
         <article>
@@ -26,5 +26,9 @@
     <!-- endforeach -->
 
 </div>
-
+<div class="my-16">
+    <a href="/links/create" class="text-sm font-semibold text-gray-900">
+        Registrar &rarr;
+    </a>
+</div>
 <?php require __DIR__ . '/partials/footer.php'; ?>
--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,5 @@ return [
     '/about' => 'app/Controllers/about.php',
     '/post'  => 'app/Controllers/post.php',
     '/links' => 'app/Controllers/links.php',
+    '/links/create' => 'app/Controllers/links_create.php',
 ];
