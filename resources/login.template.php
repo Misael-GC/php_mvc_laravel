fbb2a456bb4a02dd5d8a768e2251eeae0ffb2d45
Misael-GC <misael.gomez1623@gmail.com>
2025-11-08 13:36:09 -0600

    Vista de inicio de sesión y control de acceso a funcionalidades a usuarios sin inicio de sesión

---
+++ resources/login.template.php
@@ -0,0 +1,49 @@
+<?php partials('header.php') ?>
+
+<div class="border-b border-gray-200 pb-8 mb-8">
+    <h2 class="text-4xl font-semibold text-gray-900 sm:text-5xl text-center">Login</h2>
+</div>
+
+<div class="w-full max-w-xl mx-auto">
+    <form action="/login" method="POST">
+        <div class="mb-4">
+            <label class="text-sm font-semibold text-gray-900">Email</label>
+            <div class="mt-2">
+                <input 
+                    type="text" 
+                    name="email" 
+                    class="w-full outline-1 outline-gray-300 rounded-md px-3 py-2 text-gray-900" 
+                    value="<?= old('email')?>">
+            </div>
+        </div>
+
+        <div class="mb-4">
+            <label class="text-sm font-semibold text-gray-900">Contraseña</label>
+            <div class="mt-2">
+                <input 
+                    type="password" 
+                    name="password" 
+                    class="w-full outline-1 outline-gray-300 rounded-md px-3 py-2 text-gray-900">
+            </div>
+        </div>
+
+        <div class="mt-4">
+            <button type="submit" class="w-full rounded-md bg-indigo-600 hover:bg-indigo-500 text-white px-3 py-2 text-center text-sm font-semibold">
+                Iniciar sesión &rarr;
+            </button>
+        </div>
+    </form>
+
+    <?php if(!empty($errors)): ?>
+    <ul class="mt-4 text-red-500">
+        <!-- foreach -->
+        <?php foreach($errors as $error): ?>
+        <li class="text-xs">&rarr; <?= htmlspecialchars($error) ?></li>
+        <?php endforeach; ?>
+        <!-- endforeach -->
+    </ul>
+    <!-- endif -->
+    <?php endif; ?>
+</div>
+
+<?php partials('footer.php') ?>
