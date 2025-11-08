3db85ca0423dfe77a725491437d59a3b18011aaa
Misael-GC <misael.gomez1623@gmail.com>
2025-11-08 16:52:57 -0600

    Manejo de mensajes flash en PHP con Session Manager

--- framework/Validator.php
+++ framework/Validator.php
@@ -51,6 +51,8 @@ class Validator
     }
 
     protected function redirectIfFailed(){
+        $session = new SessionMnager();
+        $session->setFlash('errors', $this->errors);
         back();
     }
 
