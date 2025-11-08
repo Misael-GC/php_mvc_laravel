c930508e5cc4b692193d2c1f9dbe1f777b1c5fe1
Misael-GC <misael.gomez1623@gmail.com>
2025-11-08 16:06:06 -0600

    Separación de lógica de autenticación en clase

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+use Framework\Authenticate;
 use Framework\Validator;
 
 class AuthController{
@@ -16,16 +17,12 @@ class AuthController{
         ]);
 
         if($validator->passes()){
-            $user = db()->query('SELECT * FROM users WHERE email = :email',
-                ['email' => $_POST['email']]
-            )->first();
+            $login = (new Authenticate())->login(
+                $_POST['email'],
+                $_POST['password']
+            );
 
-            if($user && password_verify($_POST['password'], $user['password'])){
-                $_SESSION['user'] =[
-                    'id' => $user['id'],
-                    'email' => $user['id'],
-                    'name' => $user['name']
-                ];
+            if($login){
                 redirect('/');
             }
         }
@@ -35,8 +32,7 @@ class AuthController{
     }
 
     public function logout(){
-        unset($_SESSION['user']);
-        session_destroy();
-        redirect('/');
+        (new Authenticate())->logout();
+        redirect('/login');
     }
 }
