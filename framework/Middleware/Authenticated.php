68473185efe814b4a9bf67a5b3fd5120f3a71d44
Misael-GC <misael.gomez1623@gmail.com>
2025-11-08 00:25:00 -0600

    Middleware en PHP para proteger rutas con autenticación

---
+++ framework/Middleware/Authenticated.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Framework\Middleware;
+
+class Authenticated{
+    public function __invoke(){
+        if(!isset($_SESSION['user'])){
+            redirect('/login');
+        }
+    }
+}
